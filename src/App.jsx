552fa274b78813a2a5e9ad9e3df2e0d01f8dbd42
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-05-31 16:08:35 +0600

    update room detail page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import AddRoomPage from './pages/AddRoomPage';
 import MyListingsPage from './pages/MyListingsPage';
 import MyBookingsPage from './pages/MyBookingsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import AboutPage from './pages/AboutPage'; 
+import AboutPage from './pages/AboutPage';
 
 export default function App() {
   return (
@@ -22,7 +22,7 @@ export default function App() {
         <Route path="/rooms/:id" element={<RoomDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/about" element={<AboutPage />} />  
+        <Route path="/about" element={<AboutPage />} />
       </Route>
       <Route element={<PrivateRoute><PrivateLayout /></PrivateRoute>}>
         <Route path="/add-room" element={<AddRoomPage />} />
